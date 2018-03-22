a61e94bbf3b72f909a9187091ea87fdadc163f5f
paytham <caiyuhao2015@gmail.com>
2018-03-22 21:08:18 +0800

    创建MongoDB示例

--- src/test/kotlin/com/ld/crawler/CrawlerApplicationTests.kt
+++ src/test/kotlin/com/ld/crawler/CrawlerApplicationTests.kt
@@ -7,12 +7,10 @@ import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
-import org.springframework.test.context.web.WebAppConfiguration
 import java.util.*
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
-@WebAppConfiguration
 class CrawlerApplicationTests {
 
     @Autowired
