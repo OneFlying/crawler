3e7d700a489611a1c31b14943391d6ddddf45bab
paytham <caiyuhao2015@gmail.com>
2018-03-22 14:07:29 +0800

    创建MongoDB连接池

--- src/main/kotlin/com/ld/crawler/CrawlerApplication.kt
+++ src/main/kotlin/com/ld/crawler/CrawlerApplication.kt
@@ -1,9 +1,12 @@
 package com.ld.crawler
 
+import com.spring4all.mongodb.EnableMongoPlus
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@EnableMongoPlus
+@SpringBootApplication(exclude = [DataSourceAutoConfiguration::class])
 class CrawlerApplication
 
 fun main(args: Array<String>) {
---
+++ src/main/kotlin/com/ld/crawler/web/IndexController.kt
@@ -0,0 +1,20 @@
+package com.ld.crawler.web
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+/**
+ * 测试控制器
+ * @author Paytham (<a href="mailto:caiyuhao2015@gmail.com">caiyuhao2015@gmail.com</a>)
+ * 2018/03/22 14:04
+ */
+@RestController
+@RequestMapping("/")
+class IndexController {
+
+    @RequestMapping("/")
+    fun helloKotlin(): String {
+        return "Hello Kotlin"
+    }
+}
