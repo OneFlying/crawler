c9ed7b1b4325d289fcd3c3e7c6624686dfecc718
paytham <caiyuhao2015@gmail.com>
2018-03-25 19:01:52 +0800

    新增quartz支持

--- src/main/kotlin/com/ld/crawler/CrawlerApplication.kt
+++ src/main/kotlin/com/ld/crawler/CrawlerApplication.kt
@@ -1,14 +1,18 @@
 package com.ld.crawler
 
 import com.spring4all.mongodb.EnableMongoPlus
+import lombok.extern.slf4j.Slf4j
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication(exclude = [
     DataSourceAutoConfiguration::class
 ])
 @EnableMongoPlus
+@EnableScheduling
 class CrawlerApplication
 
     fun main(args: Array<String>) {
