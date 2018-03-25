8249816ea8c024fc34214716b9d4354c5fe6653c
paytham <caiyuhao2015@gmail.com>
2018-03-25 19:09:02 +0800

    新增quartz支持

--- src/main/kotlin/com/ld/crawler/domain/PrjInfoRepository.kt
+++ src/main/kotlin/com/ld/crawler/domain/PrjInfoRepository.kt
@@ -8,7 +8,4 @@ import org.springframework.data.mongodb.repository.MongoRepository
  * @author dzfking007 (<a href="mailto:dzfking007@163.com">dzfking007@163.com</a>)
  * 2018/3/25 12:33
  */
-interface PrjInfoRepository : MongoRepository<PrjInfo, Long> {
-
-
-}
+interface PrjInfoRepository : MongoRepository<PrjInfo, Long>
